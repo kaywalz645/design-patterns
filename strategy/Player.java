ea5ae43f84b3049e6f9f89200bd4ebfb78bc5a30
kaywalz645 <krbair0928@gmail.com>
2023-09-05 15:26:18 -0400

    started assignment 1

---
+++ strategy/Player.java
@@ -0,0 +1,33 @@
+package strategy;
+public class Player {
+    
+    private String firstName;
+    private String lastName;
+    private boolean offense;
+    protected DefenceBehavior defencebehavior;
+    protected OffenceBehavior offencebehavior;
+
+    public Player(String firstName, String lastName, boolean offence){
+
+    }
+    public void setDefenseBehavior(){
+        setDefenseBehavior();
+    }
+
+    
+    public void setOffenceBehavior(){
+        setOffenceBehavior();
+    }
+
+    public String play(){
+        //interface offense behavior
+    }
+
+    public void turnover(){
+
+    }
+    
+    public String toString(){
+
+    }
+}
