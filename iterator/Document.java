850873b0fb917d2fd9bfe2d25c4ab64206ba4952
kaywalz645 <kaybalz645@gmail.com>
2023-10-08 23:43:03 -0400

    iterator

--- iterator/Document.java
+++ iterator/Document.java
@@ -3,50 +3,81 @@ package iterator;
 import java.util.Iterator;
 import java.util.Stack;
 
+/*
+ * This class has all the methods to execute the commands of the user
+ * This class checks if the the user can make change/redo/undo
+ * @returns user changes/ undo/ redo
+ */
 public class Document {
-    private String title;
-    private Stack<String> undoStack;
-    private Stack<String> redoStack;
 
-    public Document(String title){
-        this.title = title;
-        this.redoStack = new Stack<>();
-        this.undoStack = new Stack<>();
-    }
-    public String makeChange (String change){
-        if (change !=null & !change.isEmpty()){
-            undoStack.push(change);
-            redoStack.clear();
-            return "Change Applied: " + change;
-        }else{
-            return "invalid";
-        }
-    }
-    public String undoChange(){
-        if(canUndo()){
-            undoStack.pop();
-            return "unded change";
-        }
-        return "invalid undo change";
-    }
-    public boolean canUndo(){
-        if(undoStack.size() >=0 ){
-            return true;
-        }else{
-            return false;
-        }
-    }
-    public String redoChange(){
-        if()
-    }
-    public boolean canRedo(){
+  private String title;
+  private Stack<String> undoStack = new Stack<String>();
+  private Stack<String> redoStack = new Stack<String>();
 
+  /*
+   * @returns title of document
+   */
+  public Document(String title) {
+    this.title = title;
+  }
+/*
+ * makes change in document
+ * @returns string + the change applied
+ */
+  public String makeChange(String change) {
+    undoStack.add(change);
+    return "Change Applied: " + change;
+  }
+/*
+ * undos change in document
+ * @returns the string that was undoed
+ */
+  public String undoChange() {
+    String undo = undoStack.pop();
+    redoStack.add(undo);
+    return "unded change " + undo;
+  }
+/*
+ * checks if it can undo the change / undoStack can not be null
+ * @returns boolean
+ */
+  public boolean canUndo() {
+    if (undoStack == null) {
+      return false;
+    } else {
+      return true;
     }
-    public StackIterator getUndoIterator(){
+  }
+/*
+ * redos change in document
+ * @returns the string that was unded
+ */
+  public String redoChange() {
+    if (canRedo()) {
+      String redo = redoStack.pop();
+      undoStack.add(redo);
+      return "reded change " + redo;
+    } else {
+      return "invaild redo change ";
+    }
+  }
+/*
+ * checks if it can redo the change / redoStack can not be null
+ * @returns boolean
+ */
+  public boolean canRedo() {
+    if (redoStack == null) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 
-    }
-    public StackIterator getRedoIterator(){
-        
-    }
+  public StackIterator getUndoIterator() {
+    return new StackIterator(undoStack);
+  }
 
+  public StackIterator getRedoIterator() {
+    return new StackIterator(redoStack);
+  }
 }
--- iterator/StackIterator.java
+++ iterator/StackIterator.java
@@ -1,25 +1,43 @@
 package iterator;
 
+import java.util.Iterator;
 import java.util.Stack;
 
-public class StackIterator {
-    private Stack<String> items;
-    private int position;
+/*
+ * this class looks at the next index of the stack and makes sure that there
+ * is a next then goes next if it does
+ * @returns new position of items
+ */
+public class StackIterator implements Iterator<String> {
 
-    public StackIterator(Stack<String> items) {
-        this.items = items;
-        this.position = items.size() - 1;
+  private Stack<String> items;
+  private int position;
+/*
+ * constructor of items
+ */
+  public StackIterator(Stack<String> items) {
+    this.items = items;
+  }
+/*
+ * goes to the next item
+ * @returns new position in items 
+ */
+  public String next() {
+    if (hasNext()) {
+      return items.elementAt(position++);
+    } else {
+      return null;
     }
-
-    public String next() {
-        if(hasNext()){
-            return items.get(position--);
-        }
-        return null;
+  }
+/*
+ * checks to make sure that there is a next position in items
+ * @returns boolean
+ */
+  public boolean hasNext() {
+    if (position >= items.size() || items.get(position) == null) {
+      return false;
+    } else {
+      return true;
     }
-
-    public boolean hasNext(){
-        return position >= 0;
-    }
-
+  }
 }
